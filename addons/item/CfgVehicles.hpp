8b4450eeba346f947c64a3992376364dca64d2b3
ColdEvul <andreas.brostrom.ce@gmail.com>
2017-11-26 23:25:05 +0100

    Updated texture added tempurarly placeholder texture added new laptop item

--- addons/item/CfgVehicles.hpp
+++ addons/item/CfgVehicles.hpp
@@ -34,7 +34,7 @@ class CfgVehicles {
         scopeCurator = 2;
         displayName = CSTRING(PlasticCase_01_large_Display);
         author = ECSTRING(common,BlackOrderTeam);
-        editorPreview = QPATHTOF(EditorPreviews\blackorder_Land_File2_F.jpg);
+        editorPreview = QPATHTOF(EditorPreviews\blackorder_Land_PlasticCase_01_large_F.jpg);
 
         hiddenSelectionsTextures[] = {
             QPATHTOF(data\blackorder_Land_plasticcase_01_co.paa)
@@ -47,7 +47,7 @@ class CfgVehicles {
         scopeCurator = 2;
         displayName = CSTRING(PlasticCase_01_medium_Display);
         author = ECSTRING(common,BlackOrderTeam);
-        editorPreview = QPATHTOF(EditorPreviews\blackorder_Land_File2_F.jpg);
+        editorPreview = QPATHTOF(EditorPreviews\blackorder_Land_PlasticCase_01_medium_F.jpg);
 
         hiddenSelectionsTextures[] = {
             QPATHTOF(data\blackorder_Land_plasticcase_01_co.paa)
@@ -60,28 +60,39 @@ class CfgVehicles {
         scopeCurator = 2;
         displayName = CSTRING(PlasticCase_01_small_Display);
         author = ECSTRING(common,BlackOrderTeam);
-        editorPreview = QPATHTOF(EditorPreviews\blackorder_Land_File2_F.jpg);
+        editorPreview = QPATHTOF(EditorPreviews\blackorder_Land_PlasticCase_01_small_F.jpg);
 
         hiddenSelectionsTextures[] = {
             QPATHTOF(data\blackorder_Land_plasticcase_01_co.paa)
         };
     };
-
-    class Land_Laptop_02_unfolded_F;
-    class blackorder_Land_Laptop_02_F : Land_Laptop_02_unfolded_F {
+    class Land_Laptop_unfolded_F;
+    class blackorder_Land_Laptop_01_screen_01_F : Land_Laptop_unfolded_F {
         _generalMacro = "blackorder_Land_Laptop_02_F";
         scope = 2;
         scopeCurator = 2;
-        displayName = CSTRING(Laptop_02_Display);
+        displayName = CSTRING(Laptop_01_screen_01_Display);
         author = ECSTRING(common,BlackOrderTeam);
-        editorPreview = QPATHTOF(EditorPreviews\blackorder_Land_File2_F.jpg);
+        editorPreview = QPATHTOF(EditorPreviews\blackorder_Land_Laptop_01_screen_01_co.jpg);
 
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\blackorder_laptop_02_screen_co.paa)
+            QPATHTOF(data\blackorder_Land_Laptop_01_screen_01_co.paa)
+        };
+    };
+    class Land_Laptop_02_unfolded_F;
+    class blackorder_Land_Laptop_02_screen_01_F : Land_Laptop_02_unfolded_F {
+        _generalMacro = "blackorder_Land_Laptop_02_F";
+        scope = 2;
+        scopeCurator = 2;
+        displayName = CSTRING(Laptop_01_screen_01_Display);
+        author = ECSTRING(common,BlackOrderTeam);
+        editorPreview = QPATHTOF(EditorPreviews\blackorder_Land_Laptop_02_screen_01_co.jpg);
+
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(data\blackorder_Land_Laptop_02_screen_01_co.paa)
         };
     };
 
     class Box_C_UAV_06_F;
-    class Land_Laptop_unfolded_F;
     class Land_PCSet_01_mousepad_F;
 };
