ad2ac2efc5a9dc83456bca49558bc9bbe28df93f
ColdEvul <andreas.brostrom.ce@gmail.com>
2017-12-16 09:08:45 +0100

    Fixed tabs

--- addons/vehicle_car/CfgGroups.hpp
+++ addons/vehicle_car/CfgGroups.hpp
@@ -5,149 +5,149 @@ class CfgGroups {
             class Motorized_MTP {
                 name="$STR_A3_cfggroups_East_OPF_F_Motorized_MTP0";
                 class O_BlackOrder_MotInf_Team {
-					name="$STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInfTeam0";
-					side = 0;
-					faction = "OPF_BlackOrder_F";
-					icon="\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
-					class Unit0 {
-						side = 0;
-						vehicle = "O_blackorder_veh_m1025_m2_F";
-						rank = "SERGEANT";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "O_BlackOrder_Autorifleman_F";
-						rank = "CORPORAL";
-						position[] = {5,-5,0};
-					};
-					class Unit2 {
-						side = 0;
-						vehicle = "O_BlackOrder_Soldier_AT_F";
-						rank = "PRIVATE";
-						position[] = {-5,-5,0};
-					};
-				};
-				class O_BlackOrder_MotInf_AT {
-					name="$STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInf_AT0";
-					side = 0;
-					faction = "OPF_BlackOrder_F";
-					icon="\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
-					class Unit0 {
-						side = 0;
-						vehicle = "O_blackorder_veh_m998_4dr_F";
-						rank = "SERGEANT";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "O_BlackOrder_Soldier_AT_F";
-						rank = "CORPORAL";
-						position[] = {5,-5,0};
-					};
-					class Unit2 	{
-						side = 0;
-						vehicle = "O_BlackOrder_Soldier_AT_F";
-						rank = "PRIVATE";
-						position[] = {-5,-5,0};
-					};
-					class Unit3 {
-						side = 0;
-						vehicle = "O_BlackOrder_Rifleman_2_F";
-						rank = "PRIVATE";
-						position[] = {0,-10,0};
-					};
-				};
-				class O_BlackOrder_MotInf_AA {
-					name="$STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInf_AA0";
-					side = 0;
-					faction = "OPF_BlackOrder_F";
-					icon="\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
-					class Unit0 {
-						side = 0;
-						vehicle = "O_blackorder_veh_m998_2dr_F";
-						rank = "SERGEANT";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "O_BlackOrder_Soldier_AA_F";
-						rank = "CORPORAL";
-						position[] = {5,-5,0};
-					};
-					class Unit2 {
-						side = 0;
-						vehicle = "O_BlackOrder_Soldier_AA_F";
-						rank = "PRIVATE";
-						position[] = {-5,-5,0};
-					};
-					class Unit3 {
-						side = 0;
-						vehicle = "O_BlackOrder_Rifleman_2_F";
-						rank = "PRIVATE";
-						position[] = {0,-10,0};
-					};
-				};
-				class O_BlackOrder_MotInf_MGTeam {
-					name="$STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInf_MGTeam0";
-					side = 0;
-					faction = "OPF_BlackOrder_F";
-					icon="\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
-					class Unit0 {
-						side = 0;
-						vehicle = "O_blackorder_veh_m1025_m2_F";
-						rank = "SERGEANT";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "O_BlackOrder_TeamLeader_F";
-						rank = "SERGEANT";
-						position[] = {5,-5,0};
-					};
-					class Unit2 {
-						side = 0;
-						vehicle = "O_BlackOrder_Autorifleman_F";
-						rank = "CORPORAL";
-						position[] = {-5,-5,0};
-					};
-					class Unit3 {
-						side = 0;
-						vehicle = "O_BlackOrder_Rifleman_LAT_F";
-						rank = "PRIVATE";
-						position[] = {0,-10,0};
-					};
-				};
-				class O_BlackOrder_MotInf_GMGTeam {
-					name="$STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInf_GMGTeam0";
-					side = 0;
-					faction = "OPF_BlackOrder_F";
-					icon="\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
-					class Unit0 {
-						side = 0;
-						vehicle = "O_blackorder_veh_m1025_mk19_F";
-						rank = "SERGEANT";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "O_BlackOrder_TeamLeader_F";
-						rank = "SERGEANT";
-						position[] = {5,-5,0};
-					};
-					class Unit2 {
-						side = 0;
-						vehicle = "O_BlackOrder_Autorifleman_F";
-						rank = "CORPORAL";
-						position[] = {-5,-5,0};
-					};
-					class Unit3 {
-						side = 0;
-						vehicle = "O_BlackOrder_Rifleman_LAT_F";
-						rank = "PRIVATE";
-						position[] = {0,-10,0};
-					};
-				};
+                    name="$STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInfTeam0";
+                    side = 0;
+                    faction = "OPF_BlackOrder_F";
+                    icon="\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
+                    class Unit0 {
+                        side = 0;
+                        vehicle = "O_blackorder_veh_m1025_m2_F";
+                        rank = "SERGEANT";
+                        position[] = {0,0,0};
+                    };
+                    class Unit1 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Autorifleman_F";
+                        rank = "CORPORAL";
+                        position[] = {5,-5,0};
+                    };
+                    class Unit2 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Soldier_AT_F";
+                        rank = "PRIVATE";
+                        position[] = {-5,-5,0};
+                    };
+                };
+                class O_BlackOrder_MotInf_AT {
+                    name="$STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInf_AT0";
+                    side = 0;
+                    faction = "OPF_BlackOrder_F";
+                    icon="\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
+                    class Unit0 {
+                        side = 0;
+                        vehicle = "O_blackorder_veh_m998_4dr_F";
+                        rank = "SERGEANT";
+                        position[] = {0,0,0};
+                    };
+                    class Unit1 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Soldier_AT_F";
+                        rank = "CORPORAL";
+                        position[] = {5,-5,0};
+                    };
+                    class Unit2     {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Soldier_AT_F";
+                        rank = "PRIVATE";
+                        position[] = {-5,-5,0};
+                    };
+                    class Unit3 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Rifleman_2_F";
+                        rank = "PRIVATE";
+                        position[] = {0,-10,0};
+                    };
+                };
+                class O_BlackOrder_MotInf_AA {
+                    name="$STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInf_AA0";
+                    side = 0;
+                    faction = "OPF_BlackOrder_F";
+                    icon="\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
+                    class Unit0 {
+                        side = 0;
+                        vehicle = "O_blackorder_veh_m998_2dr_F";
+                        rank = "SERGEANT";
+                        position[] = {0,0,0};
+                    };
+                    class Unit1 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Soldier_AA_F";
+                        rank = "CORPORAL";
+                        position[] = {5,-5,0};
+                    };
+                    class Unit2 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Soldier_AA_F";
+                        rank = "PRIVATE";
+                        position[] = {-5,-5,0};
+                    };
+                    class Unit3 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Rifleman_2_F";
+                        rank = "PRIVATE";
+                        position[] = {0,-10,0};
+                    };
+                };
+                class O_BlackOrder_MotInf_MGTeam {
+                    name="$STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInf_MGTeam0";
+                    side = 0;
+                    faction = "OPF_BlackOrder_F";
+                    icon="\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
+                    class Unit0 {
+                        side = 0;
+                        vehicle = "O_blackorder_veh_m1025_m2_F";
+                        rank = "SERGEANT";
+                        position[] = {0,0,0};
+                    };
+                    class Unit1 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_TeamLeader_F";
+                        rank = "SERGEANT";
+                        position[] = {5,-5,0};
+                    };
+                    class Unit2 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Autorifleman_F";
+                        rank = "CORPORAL";
+                        position[] = {-5,-5,0};
+                    };
+                    class Unit3 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Rifleman_LAT_F";
+                        rank = "PRIVATE";
+                        position[] = {0,-10,0};
+                    };
+                };
+                class O_BlackOrder_MotInf_GMGTeam {
+                    name="$STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInf_GMGTeam0";
+                    side = 0;
+                    faction = "OPF_BlackOrder_F";
+                    icon="\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
+                    class Unit0 {
+                        side = 0;
+                        vehicle = "O_blackorder_veh_m1025_mk19_F";
+                        rank = "SERGEANT";
+                        position[] = {0,0,0};
+                    };
+                    class Unit1 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_TeamLeader_F";
+                        rank = "SERGEANT";
+                        position[] = {5,-5,0};
+                    };
+                    class Unit2 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Autorifleman_F";
+                        rank = "CORPORAL";
+                        position[] = {-5,-5,0};
+                    };
+                    class Unit3 {
+                        side = 0;
+                        vehicle = "O_BlackOrder_Rifleman_LAT_F";
+                        rank = "PRIVATE";
+                        position[] = {0,-10,0};
+                    };
+                };
                 class O_BlackOrder_MotInf_Reinforce {
                     name="$STR_A3_CFGGROUPS_WEST_BLU_F_MOTORIZED_BUS_MOTINF_REINFORCE0";
                     icon = "\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
