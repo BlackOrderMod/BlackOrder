ae50c2efc882195cbd78d47d85bd03cb6dc3dbd0
ColdEvul <andreas.brostrom.ce@gmail.com>
2017-12-30 18:10:51 +0100

    Removed some macros

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -98,27 +98,3 @@
 #define MULT_OBJ_18(a) a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a
 #define MULT_OBJ_19(a) a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a
 #define MULT_OBJ_20(a) a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a
-
-// item types
-#define TYPE_DEFAULT 0
-#define TYPE_MUZZLE 101
-#define TYPE_OPTICS 201
-#define TYPE_FLASHLIGHT 301
-#define TYPE_BIPOD 302
-#define TYPE_FIRST_AID_KIT 401
-#define TYPE_FINS 501 // not implemented
-#define TYPE_BREATHING_BOMB 601 // not implemented
-#define TYPE_NVG 602
-#define TYPE_GOGGLE 603
-#define TYPE_SCUBA 604 // not implemented
-#define TYPE_HEADGEAR 605
-#define TYPE_FACTOR 607
-#define TYPE_RADIO 611
-#define TYPE_HMD 616
-#define TYPE_BINOCULAR 617
-#define TYPE_MEDIKIT 619
-#define TYPE_TOOLKIT 620
-#define TYPE_UAV_TERMINAL 621
-#define TYPE_VEST 701
-#define TYPE_UNIFORM 801
-#define TYPE_BACKPACK 901
