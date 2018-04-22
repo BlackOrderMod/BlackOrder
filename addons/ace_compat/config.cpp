a203aa12343db5dd3560e5e67f5510080decee9f
ColdEvul <andreas.brostrom.ce@gmail.com>
2018-04-22 17:46:36 +0200

    First inventroy adjustments

---
+++ addons/ace_compat/config.cpp
@@ -0,0 +1,23 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "ace_main",
+            "blackorder_main",
+
+            "blackorder_weapons"
+        };
+        author = ECSTRING(common,BlackOrderTeam);
+        authors[] = {"Brostrom.A (Evul)"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
