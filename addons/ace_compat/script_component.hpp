568beba06e0bc8bd022d4e25deda19c65ac976a3
ColdEvul <andreas.brostrom.ce@gmail.com>
2018-04-27 19:41:06 +0200

    Removed files that should not exist

--- addons/ace_compat/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT ace_compat
-#define COMPONENT_BEAUTIFIED ACE Compatibility
-
-#include "\z\blackorder\addons\main\script_mod.hpp"
-#include "\z\blackorder\addons\main\script_macros.hpp"
