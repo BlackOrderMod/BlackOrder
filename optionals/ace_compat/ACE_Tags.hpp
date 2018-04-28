f229ad8d458c43eb4ea6042101b95dc5467f4273
ColdEvul <andreas.brostrom.ce@gmail.com>
2018-04-28 23:16:04 +0200

    fixed stringtable

--- optionals/ace_compat/ACE_Tags.hpp
+++ optionals/ace_compat/ACE_Tags.hpp
@@ -1,6 +1,6 @@
 class ACE_Tags {
     class blackorder_progaganda_tagging_black {
-        displayName = CSTRING(propaganda_black);
+        displayName = CSTRING(Tag_Black_Display);
         requiredItem = "ACE_SpraypaintBlack";
         condition = QUOTE(faction player in [ARR_6('OPF_BlackOrder_F','OPF_BlackOrder_SCI_F','OPF_BlackOrder_MP_F','IND_BlackOrder_F','IND_BlackOrder_SCI_F','IND_BlackOrder_MP_F')]);
         textures[] = {
@@ -11,7 +11,7 @@ class ACE_Tags {
         icon = QPATHTOF(UI\icon_tagging_black_ca.paa);
     };
     class blackorder_progaganda_tagging_blue {
-        displayName = CSTRING(propaganda_blue);
+        displayName = CSTRING(Tag_Blue_Display);
         requiredItem = "ACE_SpraypaintBlue";
         condition = QUOTE(faction player in [ARR_6('OPF_BlackOrder_F','OPF_BlackOrder_SCI_F','OPF_BlackOrder_MP_F','IND_BlackOrder_F','IND_BlackOrder_SCI_F','IND_BlackOrder_MP_F')]);
         textures[] = {
@@ -22,7 +22,7 @@ class ACE_Tags {
         icon = QPATHTOF(UI\icon_tagging_blue_ca.paa);
     };
     class blackorder_progaganda_tagging_green {
-        displayName = CSTRING(propaganda_green);
+        displayName = CSTRING(Tag_Green_Display);
         requiredItem = "ACE_SpraypaintGreen";
         condition = QUOTE(faction player in [ARR_6('OPF_BlackOrder_F','OPF_BlackOrder_SCI_F','OPF_BlackOrder_MP_F','IND_BlackOrder_F','IND_BlackOrder_SCI_F','IND_BlackOrder_MP_F')]);
         textures[] = {
@@ -33,7 +33,7 @@ class ACE_Tags {
         icon = QPATHTOF(UI\icon_tagging_green_ca.paa);
     };
     class blackorder_progaganda_tagging_red {
-        displayName = CSTRING(propaganda_red);
+        displayName = CSTRING(Tag_Red_Display);
         requiredItem = "ACE_SpraypaintRed";
         condition = QUOTE(faction player in [ARR_6('OPF_BlackOrder_F','OPF_BlackOrder_SCI_F','OPF_BlackOrder_MP_F','IND_BlackOrder_F','IND_BlackOrder_SCI_F','IND_BlackOrder_MP_F')]);
         textures[] = {
